20235bffd87822def50f24945952ba0e8b8137b3
PaNDa2Code <moaaz0688@gmail.com>
2024-06-22 18:27:41 +0300

    Started writeup

---
+++ Writeup.md
@@ -0,0 +1,80 @@
+# MessageBox Shellcode
+
+Writing shell-code in assembly at the begging may be a little complected (at lest for me), So you may Think you can write a shell-code in c then compile it. but it's not that easy. Writing a shell-code in c may require extra steps and some knowledge in windows internals and some other tricks.
+
+In this write-up i ll show you how we can make windows MessageBox Shellcode in c.
+
+First thing you should know about is TEB "Thread Environment Block".
+
+
+> [!NOTE] Microsoft Docs
+> The Thread Environment Block (TEB) structure describes the state of a thread.
+
+
+```c
+  typedef struct _TEB {
+    PVOID Reserved1[12];
+    PPEB ProcessEnvironmentBlock;
+    PVOID Reserved2[399];
+    BYTE Reserved3[1952];
+    PVOID TlsSlots[64];
+    BYTE Reserved4[8];
+    PVOID Reserved5[26];
+    PVOID ReservedForOle;
+    PVOID Reserved6[4];
+    PVOID TlsExpansionSlots;
+  } TEB;
+  
+```
+
+
+We are only interested in `ProcessEnvironmentBlock` field in this struct.
+
+In x86_64 architecture windows holds the address of the `TEB` in spical register called `gs` (`fs` in legacy windows).
+
+`PVOID` is just a 8 bytes pointer (in x64) so the size of the first field is 8x12 = 96 bytes.
+So the pointer of the PEB is located 96 bytes away from the `PTEB` (`PTEB` + 96 = `PPEB`).
+And the `gs` register is a pointer to the current `TEB`, so `PEB` pointer is gs + 96 (`PPEB` = gs + 96).
+:
+![[diagrams/TEB.png]]
+
+So based on that. We can get the address of PEB like that:
+```asm
+mov		rax, [gs + 0x60]
+```
+
+Gnu compilers have a micro can do the same in c called `__readgsqword`:
+```c
+PPEB pPEB = __readgsqword(0x60);
+```
+
+So now we know how to get the address of `PEB`, But Why ??
+We need the `PEB` to get the address of `kernel32.dll` from the process memory which contains the function `LoadLibraryA` That we need it to load `user32.dll` which is not loaded by default like `kernel32.dll` and `ntdll.dll`.
+
+```c
+typedef struct _PEB {
+  BYTE                          Reserved1[2];
+  BYTE                          BeingDebugged;
+  BYTE                          Reserved2[1];
+  PVOID                         Reserved3[2];
+  PPEB_LDR_DATA                 Ldr;
+  PRTL_USER_PROCESS_PARAMETERS  ProcessParameters;
+  PVOID                         Reserved4[3];
+  PVOID                         AtlThunkSListPtr;
+  PVOID                         Reserved5;
+  ULONG                         Reserved6;
+  PVOID                         Reserved7;
+  ULONG                         Reserved8;
+  ULONG                         AtlThunkSListPtr32;
+  PVOID                         Reserved9[45];
+  BYTE                          Reserved10[96];
+  PPS_POST_PROCESS_INIT_ROUTINE PostProcessInitRoutine;
+  BYTE                          Reserved11[128];
+  PVOID                         Reserved12[1];
+  ULONG                         SessionId;
+} PEB, *PPEB;
+```
+
+
+
+![[diagrams/GetKernel32.dll.drawio.svg]]
--- src/c/shellcode.c
+++ src/c/shellcode.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <windows.h>
+#include <winnt.h>
 #include <winternl.h>
 
 static inline int cmpstr(char *a, char *b) {
@@ -9,6 +10,7 @@ static inline int cmpstr(char *a, char *b) {
   }
   return a[i] == 0 && b[i] == 0;
 }
+
 
 static inline PVOID getKernel32() {
   PPEB pPeb = (PPEB)__readgsqword(0x60);
@@ -23,8 +25,9 @@ static inline PVOID getKernel32() {
   return pLdrEntry->DllBase;
 }
 
-__attribute__((always_inline)) static inline PVOID FindFunction(PIMAGE_DOS_HEADER pDll, PCHAR FunctionName) {
-  PVOID RVA = (ULONGLONG)pDll + (ULONGLONG)pDll->e_lfanew;
+__attribute__((always_inline)) static inline PVOID
+FindFunction(PIMAGE_DOS_HEADER pDll, PCHAR FunctionName) {
+  PVOID RVA = (PVOID)pDll + (ULONGLONG)pDll->e_lfanew;
   PIMAGE_NT_HEADERS pNtHeaders = (PIMAGE_NT_HEADERS)RVA;
   PIMAGE_OPTIONAL_HEADER pOptHeaders = &pNtHeaders->OptionalHeader;
   PIMAGE_DATA_DIRECTORY pDataDir =
@@ -47,55 +50,54 @@ __attribute__((always_inline)) static inline PVOID FindFunction(PIMAGE_DOS_HEADE
 
 typedef PVOID(WINAPI *LoadLibraryH)(LPCSTR);
 typedef int(WINAPI *MessageBoxH)(HWND, PCHAR, PCHAR, UINT);
-typedef PVOID(WINAPI *GetProcAddressH)(HMODULE, LPCSTR);
 
-// int main() {
-//   CHAR FunNameLoadLib[] = "LoadLibraryA";
-//   CHAR FunNameMessageBox[] = "MessageBoxA";
-//   CHAR DllNameUser32[] = "user32.dll";
-//   CHAR Message[] = "HelloWorld!";
-//   CHAR Title[] = "Shellcode";
-//   LoadLibraryH LoadLibraryH = FindFunction(getKernel32(), FunNameLoadLib);
-//   PVOID pUser32Dll = LoadLibraryH(DllNameUser32);
-//   MessageBoxH MessageBoxH = FindFunction(pUser32Dll, FunNameMessageBox);
-//   MessageBoxH(NULL, Message, Title, 0);
-//   return 0;
-// }
 int main() {
   CHAR FunNameLoadLib[] = "LoadLibraryA";
   CHAR FunNameMessageBox[] = "MessageBoxA";
   CHAR DllNameUser32[] = "user32.dll";
   CHAR Message[] = "HelloWorld!";
   CHAR Title[] = "Shellcode";
-
-  PVOID rip = NULL;
-  int i = 0;
-
-  PVOID pDll = NULL;
-  PCHAR FunctionName = NULL;
-  PVOID FunctionAddr = NULL;
-  LoadLibraryH LoadLibraryH = NULL;
-  MessageBoxH MessageBoxH = NULL;
-
-  pDll = getKernel32();
-  FunctionName = FunNameLoadLib;
-  goto LfindFunction;
-L0:
-  LoadLibraryH = FunctionAddr;
-  pDll = LoadLibraryH(DllNameUser32);
-  FunctionName = FunNameMessageBox;
-  goto LfindFunction;
-L1:
-  MessageBoxH = FunctionAddr;
+  LoadLibraryH LoadLibraryH = FindFunction(getKernel32(), FunNameLoadLib);
+  PVOID pUser32Dll = LoadLibraryH(DllNameUser32);
+  MessageBoxH MessageBoxH = FindFunction(pUser32Dll, FunNameMessageBox);
   MessageBoxH(NULL, Message, Title, 0);
   return 0;
-
-LfindFunction:
-  FunctionAddr = FindFunction(pDll, FunctionName);
-  if (i == 0) {
-    ++i;
-    goto L0;
-  } else if (i == 1) {
-    goto L1;
-  }
 }
+// int main() {
+//   CHAR FunNameLoadLib[] = "LoadLibraryA";
+//   CHAR FunNameMessageBox[] = "MessageBoxA";
+//   CHAR DllNameUser32[] = "user32.dll";
+//   CHAR Message[] = "HelloWorld!";
+//   CHAR Title[] = "Shellcode";
+//
+//   PVOID rip = NULL;
+//   int i = 0;
+//
+//   PVOID pDll = NULL;
+//   PCHAR FunctionName = NULL;
+//   PVOID FunctionAddr = NULL;
+//   LoadLibraryH LoadLibraryH = NULL;
+//   MessageBoxH MessageBoxH = NULL;
+//
+//   pDll = getKernel32();
+//   FunctionName = FunNameLoadLib;
+//   goto LfindFunction;
+// L0:
+//   LoadLibraryH = FunctionAddr;
+//   pDll = LoadLibraryH(DllNameUser32);
+//   FunctionName = FunNameMessageBox;
+//   goto LfindFunction;
+// L1:
+//   MessageBoxH = FunctionAddr;
+//   MessageBoxH(NULL, Message, Title, 0);
+//   return 0;
+//
+// LfindFunction:
+//   FunctionAddr = FindFunction(pDll, FunctionName);
+//   if (i == 0) {
+//     ++i;
+//     goto L0;
+//   } else if (i == 1) {
+//     goto L1;
+//   }
+// }
